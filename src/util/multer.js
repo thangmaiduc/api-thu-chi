06888afc4f41d74e427e8e1b24567e971a72cfd8
thangmaiduc <thangmaiduc00@gmail.com>
2022-05-08 16:08:31 +0700

    fix upload

--- src/util/multer.js
+++ src/util/multer.js
@@ -7,7 +7,7 @@ module.exports = multer({
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);  
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
+      cb(new Error(`File type is not supported: ${ext}`), false);
       return;
     }
     cb(null, true);
