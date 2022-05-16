fc60d40a165110248b6bc42530ce44960c63690a
thangmaiduc <thangmaiduc00@gmail.com>
2022-05-16 14:03:24 +0700

    fix some bugs

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const swaggerFile = require('../swagger_output.json')
 var cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
+var authRouter = require('./routes/auth');
 
 
 
@@ -25,9 +25,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// dat xac thực auth trước các route khác và sau login
+// dat xac thực auth trước các route khác và sau auth
 app.use(cors())
-app.use('/auth',loginRouter
+app.use('/auth',authRouter
 // #swagger.tags = ['Auth']
 // #swagger.description = 'Endpoint for login, logout, forget password.'
     /* #swagger.security = [{
